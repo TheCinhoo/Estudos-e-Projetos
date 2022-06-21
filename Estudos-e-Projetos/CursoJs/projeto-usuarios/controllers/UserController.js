7565b9a26254abf3746752e22ab07740cd3c635e
Fabrício Valtair da Silva <thecinhoo@gmail.com>
2022-06-21 20:35:38 -0300

    Mais um dia vencido

---
+++ Estudos-e-Projetos/CursoJs/projeto-usuarios/controllers/UserController.js
@@ -0,0 +1,82 @@
+class UserController{
+
+    constructor(formId, tabelId) {
+        
+        this.formEl  = document.getElementById(formId);
+        this.tableEl = document.getElementById(tabelId);
+
+        this.onSubmit();
+
+    } //Close Constructor
+
+
+    onSubmit() {
+        
+
+       this.formEl.addEventListener("submit", event => {
+    
+           event.preventDefault();
+           
+           this.addLine(this.getValues());
+        
+        });
+
+    } //Close onSubmit
+
+
+    getValues() {
+
+        let user = {};
+
+        this.formEl.elements.forEach(function(field, index) {
+    
+            if (field.name == "gender") {
+                
+                if (field.checked) {
+                
+                    user[field.name] = field.value;
+                
+                }
+        
+            } else {
+                
+                user[field.name] = field.value;
+        
+            }
+            
+        });
+    
+        return new User(
+            user.name,
+            user.gender,
+            user.birth,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
+            user.admin
+        );
+    
+    } //Close getValues
+
+
+    addLine(dataUser) {
+
+        this.tableEl.innerHTML = `
+        <tr>
+            <td>
+            <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm" />
+            </td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+        `;
+    
+    } //Close addLine
+}
