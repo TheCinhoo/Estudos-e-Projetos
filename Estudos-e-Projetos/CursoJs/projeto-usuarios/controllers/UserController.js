c4f6122b151e1c75a78d95fbbedf53e29c92ea82
Fabrício Valtair da Silva <thecinhoo@gmail.com>
2022-06-27 21:51:06 -0300

    Aula 27-6-2022

--- Estudos-e-Projetos/CursoJs/projeto-usuarios/controllers/UserController.js
+++ Estudos-e-Projetos/CursoJs/projeto-usuarios/controllers/UserController.js
@@ -1,82 +1,154 @@
-class UserController{
+class UserController {
 
-    constructor(formId, tabelId) {
-        
-        this.formEl  = document.getElementById(formId);
-        this.tableEl = document.getElementById(tabelId);
+    constructor (formId, tableId){
 
-        this.onSubmit();
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
 
-    } //Close Constructor
+        this.onSubmit()
 
+    }
 
-    onSubmit() {
-        
+    onSubmit(){
 
-       this.formEl.addEventListener("submit", event => {
-    
-           event.preventDefault();
-           
-           this.addLine(this.getValues());
+        this.formEl.addEventListener("submit", event => {
+
+            event.preventDefault();
+
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
+            let values = this.getValues();
+
+            if (!values) return false;
+
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
+
+                }, 
+                (e) => {
+                    console.error(e)
+                }
+            );
         
         });
 
-    } //Close onSubmit
+    }
 
+    getPhoto(){
 
-    getValues() {
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+                    return item;
+                }
+
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+
+                reject(e);
+
+            };
+
+            if(file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
+
+        });
+
+    }
+
+    getValues(){
 
         let user = {};
+        let isValid = true;
 
-        this.formEl.elements.forEach(function(field, index) {
-    
-            if (field.name == "gender") {
-                
-                if (field.checked) {
-                
-                    user[field.name] = field.value;
-                
-                }
-        
-            } else {
-                
-                user[field.name] = field.value;
-        
+        [...this.formEl.elements].forEach(function(field, index){
+
+            if (["name", "email", "password"].indexOf(field.name) > -1 && !field.value) {
+                field.parentElement.classList.add('has-error');
+                isValid = false;
             }
-            
-        });
+
+            if (field.name === "gender") {
     
+                if (field.checked) {
+                    user[field.name] = field.value
+                }
+    
+            } else if(field.name == "admin") {
+
+                user[field.name] = field.checked;
+
+            } else {
+    
+                user[field.name] = field.value
+    
+            }
+    
+        });
+        
+        if (!isValid) {
+            return false;
+        }
+
         return new User(
-            user.name,
-            user.gender,
-            user.birth,
-            user.country,
-            user.email,
-            user.password,
-            user.photo,
+            user.name, 
+            user.gender, 
+            user.birth, 
+            user.country, 
+            user.email, 
+            user.password, 
+            user.photo, 
             user.admin
         );
+
+    }
+
     
-    } //Close getValues
-
-
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-        <tr>
-            <td>
-            <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm" />
-            </td>
-            <td>${dataUser.name}</td>
-            <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
-            <td>${dataUser.birth}</td>
-            <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-            </td>
-        </tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
+            <tr>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+                <td>${Utils.dateFormat(dataUser.register)}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
         `;
-    
-    } //Close addLine
+
+        this.tableEl.appendChild(tr);
+
+    }
 }
